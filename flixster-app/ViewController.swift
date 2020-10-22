620abd37094797f058bc5e76021183bd754f4340
armankabir <47779642+armankabir@users.noreply.github.com>
2020-10-22 09:42:39 -0400

    Initial Commit

---
+++ flixster-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixster-app
+//
+//  Created by Arman Kabir on 10/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
